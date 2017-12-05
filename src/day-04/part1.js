645c8073bf458e954245b2fce5724e3440025b0b
Iain M Norman <iain@norman.org>
2017-12-05 08:25:20 +0000

    4

---
+++ src/day-04/part1.js
@@ -0,0 +1,22 @@
+var _ = require('lodash');
+
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('input.txt')
+});
+
+var output = 0;
+
+function validPassword(array){
+    return _.uniq(array).length == array.length;
+}
+
+lineReader.on('line', function (line) {
+    items = line.split(' ');
+    if (validPassword(items)){
+        output++;
+    }
+});
+
+lineReader.on('close', function (line) {
+    console.log(output);
+});
